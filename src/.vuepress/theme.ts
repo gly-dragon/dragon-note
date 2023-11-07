1337070fa9d2ccae34fc5a3991c3f194eb75d89b
高龙运 <gly3498347531@163.com>
2023-11-07 14:32:20 +0800

    初始化

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -1,179 +1,184 @@
-import { hopeTheme } from "vuepress-theme-hope";
+import {hopeTheme} from "vuepress-theme-hope";
 import navbar from "./navbar.js";
 import sidebar from "./sidebar.js";
 
 export default hopeTheme({
-  hostname: "https://vuepress-theme-hope-docs-demo.netlify.app",
-  // 不显示贡献者信息
-  contributors:false,
-  author: {
-    name: "Mr.Hope",
-    url: "https://mister-hope.com",
-  },
+	hostname: "https://vuepress-theme-hope-docs-demo.netlify.app",
+	// 不显示贡献者信息
+	contributors: false,
+	// 默认作者信息
+	author: {
+		name: "龙哥不管事",
+		// 作者网站
+		// url: "https://mister-hope.com",
+	},
+	// 站点图标
+	// favicon: '',
+	iconAssets: "fontawesome-with-brands",
 
-  iconAssets: "fontawesome-with-brands",
+	logo: "/logo.svg",
+	// 仓库地址 username/repo
+	repo: "gly-dragon/dragon-note",
+	// 文档在仓库中的目录
+	docsDir: "src",
+	// 文档存放的分支
+	docsBranch: 'main',
+	// navbar
+	navbar,
+	// sidebar
+	sidebar,
 
-  logo: "/logo.svg",
+	footer: "默认页脚",
 
-  repo: "vuepress-theme-hope/vuepress-theme-hope",
+	displayFooter: true,
 
-  docsDir: "src",
+	encrypt: {
+		config: {
+			"/demo/encrypt.html": ["1234"],
+		},
+	},
 
-  // navbar
-  navbar,
+	// page meta
+	metaLocales: {
+		editLink: "在 GitHub 上编辑此页",
+	},
 
-  // sidebar
-  sidebar,
+	plugins: {
+		// You should generate and use your own comment service
+		comment: {
+			// 关闭评论功能
+			comment: false,
+			provider: "Giscus",
+			repo: "vuepress-theme-hope/giscus-discussions",
+			repoId: "R_kgDOG_Pt2A",
+			category: "Announcements",
+			categoryId: "DIC_kwDOG_Pt2M4COD69",
+		},
 
-  footer: "默认页脚",
+		// All features are enabled for demo, only preserve features you need here
+		mdEnhance: {
+			align: true,
+			attrs: true,
+			card: true,
 
-  displayFooter: true,
+			// install chart.js before enabling it
+			// chart: true,
 
-  encrypt: {
-    config: {
-      "/demo/encrypt.html": ["1234"],
-    },
-  },
+			codetabs: true,
+			demo: true,
 
-  // page meta
-  metaLocales: {
-    editLink: "在 GitHub 上编辑此页",
-  },
+			// install echarts before enabling it
+			// echarts: true,
 
-  plugins: {
-    // You should generate and use your own comment service
-    comment: {
-      provider: "Giscus",
-      repo: "vuepress-theme-hope/giscus-discussions",
-      repoId: "R_kgDOG_Pt2A",
-      category: "Announcements",
-      categoryId: "DIC_kwDOG_Pt2M4COD69",
-    },
+			figure: true,
 
-    // All features are enabled for demo, only preserve features you need here
-    mdEnhance: {
-      align: true,
-      attrs: true,
-      card: true,
+			// install flowchart.ts before enabling it
+			// flowchart: true,
 
-      // install chart.js before enabling it
-      // chart: true,
+			// gfm requires mathjax-full to provide tex support
+			// gfm: true,
 
-      codetabs: true,
-      demo: true,
+			imgLazyload: true,
+			imgSize: true,
+			include: true,
 
-      // install echarts before enabling it
-      // echarts: true,
+			// install katex before enabling it
+			// katex: true,
 
-      figure: true,
+			// install mathjax-full before enabling it
+			// mathjax: true,
 
-      // install flowchart.ts before enabling it
-      // flowchart: true,
+			mark: true,
 
-      // gfm requires mathjax-full to provide tex support
-      // gfm: true,
+			// install mermaid before enabling it
+			// mermaid: true,
 
-      imgLazyload: true,
-      imgSize: true,
-      include: true,
+			playground: {
+				presets: ["ts", "vue"],
+			},
 
-      // install katex before enabling it
-      // katex: true,
+			// install reveal.js before enabling it
+			// revealJs: {
+			//   plugins: ["highlight", "math", "search", "notes", "zoom"],
+			// },
 
-      // install mathjax-full before enabling it
-      // mathjax: true,
+			stylize: [
+				{
+					matcher: "Recommended",
+					replacer: ({tag}) => {
+						if(tag === "em")
+							return {
+								tag: "Badge",
+								attrs: {type: "tip"},
+								content: "Recommended",
+							};
+					},
+				},
+			],
+			sub: true,
+			sup: true,
+			tabs: true,
+			vPre: true,
 
-      mark: true,
+			// install @vue/repl before enabling it
+			// vuePlayground: true,
+		},
 
-      // install mermaid before enabling it
-      // mermaid: true,
-
-      playground: {
-        presets: ["ts", "vue"],
-      },
-
-      // install reveal.js before enabling it
-      // revealJs: {
-      //   plugins: ["highlight", "math", "search", "notes", "zoom"],
-      // },
-
-      stylize: [
-        {
-          matcher: "Recommended",
-          replacer: ({ tag }) => {
-            if (tag === "em")
-              return {
-                tag: "Badge",
-                attrs: { type: "tip" },
-                content: "Recommended",
-              };
-          },
-        },
-      ],
-      sub: true,
-      sup: true,
-      tabs: true,
-      vPre: true,
-
-      // install @vue/repl before enabling it
-      // vuePlayground: true,
-    },
-
-    // uncomment these if you want a pwa
-    // pwa: {
-    //   favicon: "/favicon.ico",
-    //   cacheHTML: true,
-    //   cachePic: true,
-    //   appendBase: true,
-    //   apple: {
-    //     icon: "/assets/icon/apple-icon-152.png",
-    //     statusBarColor: "black",
-    //   },
-    //   msTile: {
-    //     image: "/assets/icon/ms-icon-144.png",
-    //     color: "#ffffff",
-    //   },
-    //   manifest: {
-    //     icons: [
-    //       {
-    //         src: "/assets/icon/chrome-mask-512.png",
-    //         sizes: "512x512",
-    //         purpose: "maskable",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-mask-192.png",
-    //         sizes: "192x192",
-    //         purpose: "maskable",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-512.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "/assets/icon/chrome-192.png",
-    //         sizes: "192x192",
-    //         type: "image/png",
-    //       },
-    //     ],
-    //     shortcuts: [
-    //       {
-    //         name: "Demo",
-    //         short_name: "Demo",
-    //         url: "/demo/",
-    //         icons: [
-    //           {
-    //             src: "/assets/icon/guide-maskable.png",
-    //             sizes: "192x192",
-    //             purpose: "maskable",
-    //             type: "image/png",
-    //           },
-    //         ],
-    //       },
-    //     ],
-    //   },
-    // },
-  },
+		// uncomment these if you want a pwa
+		// pwa: {
+		//   favicon: "/favicon.ico",
+		//   cacheHTML: true,
+		//   cachePic: true,
+		//   appendBase: true,
+		//   apple: {
+		//     icon: "/assets/icon/apple-icon-152.png",
+		//     statusBarColor: "black",
+		//   },
+		//   msTile: {
+		//     image: "/assets/icon/ms-icon-144.png",
+		//     color: "#ffffff",
+		//   },
+		//   manifest: {
+		//     icons: [
+		//       {
+		//         src: "/assets/icon/chrome-mask-512.png",
+		//         sizes: "512x512",
+		//         purpose: "maskable",
+		//         type: "image/png",
+		//       },
+		//       {
+		//         src: "/assets/icon/chrome-mask-192.png",
+		//         sizes: "192x192",
+		//         purpose: "maskable",
+		//         type: "image/png",
+		//       },
+		//       {
+		//         src: "/assets/icon/chrome-512.png",
+		//         sizes: "512x512",
+		//         type: "image/png",
+		//       },
+		//       {
+		//         src: "/assets/icon/chrome-192.png",
+		//         sizes: "192x192",
+		//         type: "image/png",
+		//       },
+		//     ],
+		//     shortcuts: [
+		//       {
+		//         name: "Demo",
+		//         short_name: "Demo",
+		//         url: "/demo/",
+		//         icons: [
+		//           {
+		//             src: "/assets/icon/guide-maskable.png",
+		//             sizes: "192x192",
+		//             purpose: "maskable",
+		//             type: "image/png",
+		//           },
+		//         ],
+		//       },
+		//     ],
+		//   },
+		// },
+	},
 });
